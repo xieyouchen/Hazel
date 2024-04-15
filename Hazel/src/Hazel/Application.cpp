25dedf3ec345418aad22e0d7cad0b58bd5a8b78f
xieyouchen <859666356@qq.com>
2024-04-15 21:29:10 +0800

    Setup basic Application and EntryPoint

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,16 @@
+#include "Application.h"
+
+namespace Hazel {
+	Hazel::Application::Application()
+	{
+	}
+
+	Hazel::Application::~Application()
+	{
+	}
+
+	void Hazel::Application::Run()
+	{
+		while (true);
+	}
+}
